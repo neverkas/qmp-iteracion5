92f5e6651d336006255678430440dc04e5fb5a5f
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-05-13 09:42:42 -0300

    Agregamos override faltante

--- src/main/java/dds/monedero/model/MovimientoDeposito.java
+++ src/main/java/dds/monedero/model/MovimientoDeposito.java
@@ -16,7 +16,8 @@ public class MovimientoDeposito extends Movimiento{
   public double calcularValor(Cuenta cuenta) {
     return cuenta.getSaldo() + getMonto();  	
   }
-
+  
+  @Override
   public void agregateA(Cuenta cuenta) {
   	cuenta.setSaldo(calcularValor(cuenta));
     cuenta.agregarDeposito(this.getFecha(), this.getMonto());
